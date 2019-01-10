0dfecbefe6fbfab8dd75f2bd603ff9a09982f041
Victor Lewon <viclew@gmail.com>
2019-01-10 21:21:03 +0100

    Correction loading natives OpenCV

--- src/fr/lewon/dofus/fm/imageprocessing/ImageOCR.java
+++ src/fr/lewon/dofus/fm/imageprocessing/ImageOCR.java
@@ -9,10 +9,10 @@ import javax.imageio.ImageIO;
 
 import org.opencv.core.CvType;
 import org.opencv.core.Mat;
-import org.opencv.imgcodecs.Imgcodecs;
 import org.opencv.imgproc.Imgproc;
 
 import fr.lewon.dofus.fm.common.CaracType;
+import nu.pattern.OpenCV;
 
 public enum ImageOCR {
 
@@ -24,32 +24,34 @@ public enum ImageOCR {
 		Mat imgMat = matify(img);
 		Mat imgThresholdMat = new Mat();
 		Imgproc.threshold(imgMat, imgThresholdMat, 0, 255, Imgproc.ADAPTIVE_THRESH_GAUSSIAN_C);
-		Imgcodecs.imwrite("test.jpg",imgThresholdMat);
+		//		Imgcodecs.imwrite("test.jpg",imgThresholdMat);
 		return null;
 	}
 
 	// Convert image to Mat
-	public static Mat matify(BufferedImage im) {
-	    // Convert INT to BYTE
-	    //im = new BufferedImage(im.getWidth(), im.getHeight(),BufferedImage.TYPE_3BYTE_BGR);
-	    // Convert bufferedimage to byte array
-	    byte[] pixels = ((DataBufferByte) im.getRaster().getDataBuffer())
-	            .getData();
+	public Mat matify(BufferedImage im) {
+		// Convert INT to BYTE
+		//im = new BufferedImage(im.getWidth(), im.getHeight(),BufferedImage.TYPE_3BYTE_BGR);
+		// Convert bufferedimage to byte array
+		byte[] pixels = ((DataBufferByte) im.getRaster().getDataBuffer())
+				.getData();
 
-	    // Create a Matrix the same size of image
-	    Mat image = new Mat(im.getHeight(), im.getWidth(), CvType.CV_8UC3);
-	    // Fill Matrix with image values
-	    image.put(0, 0, pixels);
+		// Create a Matrix the same size of image
+		Mat image = new Mat(im.getHeight(), im.getWidth(), CvType.CV_8UC3);
+		// Fill Matrix with image values
+		image.put(0, 0, pixels);
 
-	    return image;
+		return image;
 
 	}
-	
+
 	public static void main(String[] args){
+
+		OpenCV.loadShared();
 		BufferedImage img = null;
 		try {
-		    img = ImageIO.read(new File("imgTest/vertSURrouge.png"));
-		    ImageOCR.INSTANCE.getCaracType(img);
+			img = ImageIO.read(new File("imgTest/vertSURrouge.png"));
+			ImageOCR.INSTANCE.getCaracType(img);
 		} catch (IOException e) {
 		}
 	}
